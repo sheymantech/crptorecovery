3077c4bf812f0b353b6908c067b3232c7839d67d
sheymantech <olaniyansheyi1704@gmail.com>
2024-05-28 15:14:15 +0100

    new feature

--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -1,7 +1,7 @@
 function Hero() {
   return (
     <div className="lg:px-32 px-6 flex flex-wrap justify-center items-center gap-5 w-full text-left ">
-      <div className="flex flex-col gap-y-4">
+      <div className="flex flex-col gap-y-4 py-14">
         <h1 className="font-bold text-4xl tracking-wide text-gray-800">
           Easy, Transparent Wallet Recovery
         </h1>
