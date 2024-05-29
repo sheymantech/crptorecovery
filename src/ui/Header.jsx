356181e9c990db222048610743312c0b6a85adc2
sheymantech <olaniyansheyi1704@gmail.com>
2024-05-29 14:47:05 +0100

    new feature

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -36,13 +36,15 @@ function Header() {
     <>
       <div
         className={`flex justify-between items-center lg:px-32 pt-5 px-6 h-auto flex-wrap ${
-          openMenu ? "justify-start text-left pb-14" : ""
+          openMenu ? " text-left pb-14" : ""
         }`}
       >
         <div className="w-[10rem]">
-          <img className="w-full" src={logo} alt="" />
+          <Link onClick={() => setOpenMenu(false)} to="/home">
+            <img className="w-full" src={logo} alt="" />
+          </Link>
         </div>
-        <div className="sm:hidden" onClick={toggleMenu}>
+        <div className="sm:hidden ms-auto" onClick={toggleMenu}>
           {openMenu ? (
             <RxCross1 className="font-bold text-3xl text-black" />
           ) : (
@@ -56,7 +58,14 @@ function Header() {
               : "hidden"
           }`}
         >
-          <p className=" mr-auto">Home</p>
+          <Link
+            className="mr-auto"
+            onClick={() => setOpenMenu(false)}
+            to="/home"
+          >
+            {" "}
+            <p className=" mr-auto">Home</p>
+          </Link>
           <p
             onClick={toggleResources}
             onMouseEnter={toggleResources}
@@ -77,54 +86,63 @@ function Header() {
                   openResources ? "h-auto gap-y-5 bg-white z-50 top-10" : ""
                 }`}
               >
-                <div className="flex flex-none items-center justify-center gap-x-5">
-                  <span className="p-3 sm:p-2 rounded-full flex justify-center items-center bg-[#8bb4e9] sm:hidden">
-                    <FaFlag className="text-[#026ffa] sm:text-lg text-2xl font-semibold  " />
-                  </span>
-                  <span className="text-left">
-                    <h3 className="text-sm">About us</h3>
-                    <p className="sm:hidden text-sm font-normal">
-                      learn about our story and our mission statement.
-                    </p>
-                  </span>
-                </div>
-                <div className="flex flex-none items-center justify-center gap-x-5">
-                  <span className="p-3 sm:p-2 rounded-full flex justify-center items-center bg-[#8bb4e9] sm:hidden">
-                    <FiMessageCircle className="text-[#026ffa] sm:text-lg text-2xl font-semibold  " />
-                  </span>
-                  <span className="text-left">
-                    <h3 className="text-sm">Press</h3>
-                    <p className="sm:hidden text-sm font-normal">
-                      news and writings, press releases and resources.
-                    </p>
-                  </span>
-                </div>
-                <div className="flex flex-none items-center justify-center gap-x-5">
-                  <span className="p-3 sm:p-2 rounded-full flex justify-center items-center bg-[#8bb4e9] sm:hidden">
-                    <IoIosContacts className="text-[#026ffa] sm:text-lg text-2xl font-semibold  " />
-                  </span>
-                  <span className="text-left">
-                    <h3 className="text-sm">Testimonials</h3>
-                    <p className="sm:hidden text-sm font-normal">
-                      Read reviews from some of our former clients.
-                    </p>
-                  </span>
-                </div>
-                <div className="flex flex-none items-center justify-center gap-x-5">
-                  <span className="p-3 sm:p-2 rounded-full flex justify-center items-center bg-[#8bb4e9] sm:hidden">
-                    <FaFileInvoice className="text-[#026ffa] sm:text-lg text-2xl font-semibold  " />
-                  </span>
-                  <span className="text-left">
-                    <h3 className="text-sm">Knowledge center</h3>
-                    <p className="sm:hidden text-sm font-normal">
-                      Your go to place to learn more about wallet recovery.
-                    </p>
-                  </span>
-                </div>
-
-                <button className=" mt-2 w-full py-2 px-2 text-center bg-[#eff1fa] rounded-lg text-[#0e34b2] font-semibold tracking-wide cursor-pointer">
-                  recover your wallet
-                </button>
+                <Link onClick={() => setOpenMenu(false)} to="/about">
+                  <div className="flex flex-none items-center justify-center gap-x-5">
+                    <span className="p-3 sm:p-2 rounded-full flex justify-center items-center bg-[#8bb4e9] sm:hidden">
+                      <FaFlag className="text-[#026ffa] sm:text-lg text-2xl font-semibold  " />
+                    </span>
+                    <span className="text-left">
+                      <h3 className="text-sm">About us</h3>
+                      <p className="sm:hidden text-sm font-normal">
+                        learn about our story and our mission statement.
+                      </p>
+                    </span>
+                  </div>
+                </Link>
+                <Link onClick={() => setOpenMenu(false)} to="/press">
+                  <div className="flex flex-none items-center justify-center gap-x-5">
+                    <span className="p-3 sm:p-2 rounded-full flex justify-center items-center bg-[#8bb4e9] sm:hidden">
+                      <FiMessageCircle className="text-[#026ffa] sm:text-lg text-2xl font-semibold  " />
+                    </span>
+                    <span className="text-left">
+                      <h3 className="text-sm">Press</h3>
+                      <p className="sm:hidden text-sm font-normal">
+                        news and writings, press releases and resources.
+                      </p>
+                    </span>
+                  </div>
+                </Link>
+                <Link onClick={() => setOpenMenu(false)} to="testimonial">
+                  <div className="flex flex-none items-center justify-center gap-x-5">
+                    <span className="p-3 sm:p-2 rounded-full flex justify-center items-center bg-[#8bb4e9] sm:hidden">
+                      <IoIosContacts className="text-[#026ffa] sm:text-lg text-2xl font-semibold  " />
+                    </span>
+                    <span className="text-left">
+                      <h3 className="text-sm">Testimonials</h3>
+                      <p className="sm:hidden text-sm font-normal">
+                        Read reviews from some of our former clients.
+                      </p>
+                    </span>
+                  </div>
+                </Link>
+                <Link onClick={() => setOpenMenu(false)} to="knowledge-center">
+                  <div className="flex flex-none items-center justify-center gap-x-5">
+                    <span className="p-3 sm:p-2 rounded-full flex justify-center items-center bg-[#8bb4e9] sm:hidden">
+                      <FaFileInvoice className="text-[#026ffa] sm:text-lg text-2xl font-semibold  " />
+                    </span>
+                    <span className="text-left">
+                      <h3 className="text-sm">Knowledge center</h3>
+                      <p className="sm:hidden text-sm font-normal">
+                        Your go to place to learn more about wallet recovery.
+                      </p>
+                    </span>
+                  </div>
+                </Link>
+                <Link onClick={() => setOpenMenu(false)} to="/contact">
+                  <button className=" mt-2 w-full py-2 px-2 text-center bg-[#eff1fa] rounded-lg text-[#0e34b2] font-semibold tracking-wide cursor-pointer">
+                    recover your wallet
+                  </button>
+                </Link>
               </div>
             )}
           </p>
@@ -148,92 +166,123 @@ function Header() {
                   openWallet ? "h-auto gap-y-5 bg-white z-50 top-10" : ""
                 }`}
               >
-                <div className="flex flex-none items-center justify-center gap-x-5">
-                  <div className="p-3 sm:p-2 rounded-full flex justify-center items-center sm:hidden">
-                    <img src={blockchain} alt="" />
+                <Link
+                  onClick={() => setOpenMenu(false)}
+                  to="/wallets/blockchain-com"
+                >
+                  <div className="flex flex-none items-center justify-center gap-x-5">
+                    <div className="p-3 sm:p-2 rounded-full flex justify-center items-center sm:hidden">
+                      <img src={blockchain} alt="" />
+                    </div>
+                    <span className="text-left">
+                      <h3 className="text-sm">Blockchain.com</h3>
+                      <p className="sm:hidden text-sm font-normal">
+                        learn about our Blockchain wallet.
+                      </p>
+                    </span>
                   </div>
-                  <span className="text-left">
-                    <h3 className="text-sm">Blockchain.com</h3>
-                    <p className="sm:hidden text-sm font-normal">
-                      learn about our Blockchain wallet.
-                    </p>
-                  </span>
-                </div>
+                </Link>
 
-                <div className="flex flex-none items-center justify-center gap-x-5">
-                  <div className="p-3 sm:p-2 rounded-full flex justify-center items-center sm:hidden">
-                    <img src={bitcoin} alt="" />
+                <Link
+                  onClick={() => setOpenMenu(false)}
+                  to="/wallets/bitcoin-core"
+                >
+                  <div className="flex flex-none items-center justify-center gap-x-5">
+                    <div className="p-3 sm:p-2 rounded-full flex justify-center items-center sm:hidden">
+                      <img src={bitcoin} alt="" />
+                    </div>
+                    <span className="text-left">
+                      <h3 className="text-sm">Bitcoin core</h3>
+                      <p className="sm:hidden text-sm font-normal">
+                        learn about our Bitcoin core wallet.
+                      </p>
+                    </span>
                   </div>
-                  <span className="text-left">
-                    <h3 className="text-sm">Bitcoin core</h3>
-                    <p className="sm:hidden text-sm font-normal">
-                      learn about our Bitcoin core wallet.
-                    </p>
-                  </span>
-                </div>
-                <div className="flex flex-none items-center justify-center gap-x-5">
-                  <div className="p-3 sm:p-2 rounded-full flex justify-center items-center sm:hidden">
-                    <img src={multibit} alt="" />
+                </Link>
+                <Link onClick={() => setOpenMenu(false)} to="/wallets/multibit">
+                  <div className="flex  items-center justify-center gap-x-5">
+                    <div className="p-3 sm:p-2 rounded-full flex justify-center items-center sm:hidden">
+                      <img src={multibit} alt="" />
+                    </div>
+                    <span className="text-left">
+                      <h3 className="text-sm">Multibit wallet</h3>
+                      <p className="sm:hidden text-sm font-normal">
+                        learn about our multibit wallet.
+                      </p>
+                    </span>
                   </div>
-                  <span className="text-left">
-                    <h3 className="text-sm">Multibit wallet</h3>
-                    <p className="sm:hidden text-sm font-normal">
-                      learn about our multibit wallet.
-                    </p>
-                  </span>
-                </div>
-                <div className="flex flex-none items-center justify-center gap-x-5">
-                  <div className="p-3 sm:p-2 rounded-full flex justify-center items-center sm:hidden">
-                    <img src={metamask} alt="" />
+                </Link>
+                <Link onClick={() => setOpenMenu(false)} to="/wallets/metamask">
+                  <div className="flex flex-none items-center justify-center gap-x-5">
+                    <div className="p-3 sm:p-2 rounded-full flex justify-center items-center sm:hidden">
+                      <img src={metamask} alt="" />
+                    </div>
+                    <span className="text-left">
+                      <h3 className="text-sm">Metamask Wallet</h3>
+                      <p className="sm:hidden text-sm font-normal">
+                        learn about our Metamask wallet.
+                      </p>
+                    </span>
                   </div>
-                  <span className="text-left">
-                    <h3 className="text-sm">Metamask Wallet</h3>
-                    <p className="sm:hidden text-sm font-normal">
-                      learn about our Metamask wallet.
-                    </p>
-                  </span>
-                </div>
-                <div className="flex flex-none items-center justify-center gap-x-5">
-                  <div className="p-3 sm:p-2 rounded-full flex justify-center items-center sm:hidden">
-                    <img src={electrum} alt="" />
+                </Link>
+                <Link onClick={() => setOpenMenu(false)} to="/wallets/electrum">
+                  <div className="flex flex-none items-center justify-center gap-x-5">
+                    <div className="p-3 sm:p-2 rounded-full flex justify-center items-center sm:hidden">
+                      <img src={electrum} alt="" />
+                    </div>
+                    <span className="text-left">
+                      <h3 className="text-sm">Electrum</h3>
+                      <p className="sm:hidden text-sm font-normal">
+                        learn about our Electrum wallet.
+                      </p>
+                    </span>
                   </div>
-                  <span className="text-left">
-                    <h3 className="text-sm">Electrum</h3>
-                    <p className="sm:hidden text-sm font-normal">
-                      learn about our Electrum wallet.
-                    </p>
-                  </span>
-                </div>
-
-                <button className=" mt-2 w-full py-2 px-2 text-center bg-[#eff1fa] rounded-lg text-[#0e34b2] font-semibold tracking-wide cursor-pointer">
-                  recover your wallet
-                </button>
+                </Link>
+                <Link onClick={() => setOpenMenu(false)} to="/contact">
+                  <button className=" mt-2 w-full py-2 px-2 text-center bg-[#eff1fa] rounded-lg text-[#0e34b2] font-semibold tracking-wide cursor-pointer">
+                    recover your wallet
+                  </button>
+                </Link>
               </div>
             )}
           </p>
-          <p className="mr-auto">Pricing</p>
-          <p className="mr-auto">Scam Tracing</p>
+          <Link
+            className="mr-auto"
+            onClick={() => setOpenMenu(false)}
+            to="/pricing"
+          >
+            {" "}
+            <p className="mr-auto">Pricing</p>
+          </Link>
+          <Link
+            className="mr-auto"
+            onClick={() => setOpenMenu(false)}
+            to="/scam-trace"
+          >
+            <p className="mr-auto">Scam Tracing</p>
+          </Link>
         </nav>
-
         {openMenu && (
+          <Link onClick={() => setOpenMenu(false)} to="/contact">
+            <button
+              className={`bg-white rounded-lg font-semibold border-2 border-gray-600 tracking-wide text-gray-600 mb-5 py-2 px-5 text-lg sm:flex  ${
+                openMenu ? "flex w-full justify-center" : "hidden"
+              }`}
+            >
+              Login
+            </button>
+          </Link>
+        )}
+        <Link onClick={() => setOpenMenu(false)} to="/contact">
           <button
-            className={`bg-white rounded-lg font-semibold border-2 border-gray-600 tracking-wide text-gray-600 mb-5 py-2 px-5 text-lg sm:flex  ${
+            className={`bg-[#026ffa] hover:bg-[#0e34b2] rounded-lg font-semibold tracking-wide text-white py-2 px-5 text-lg sm:flex  ${
               openMenu ? "flex w-full justify-center" : "hidden"
             }`}
           >
             {" "}
-            Login
+            Contact us
           </button>
-        )}
-
-        <button
-          className={`bg-[#026ffa] hover:bg-[#0e34b2] rounded-lg font-semibold tracking-wide text-white py-2 px-5 text-lg sm:flex  ${
-            openMenu ? "flex w-full justify-center" : "hidden"
-          }`}
-        >
-          {" "}
-          Contact us
-        </button>
+        </Link>
       </div>
     </>
   );
